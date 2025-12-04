fc988bf63f39724cc00e94e02b35dbd2fa465077
udaykumar2507 <uk25072004@gmail.com>
2025-12-04 22:29:59 +0530

    Added frontend changes

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // change later if needed
+  baseURL: 'https://skillswap-u8kg.onrender.com', // change later if needed
 });
 
 api.interceptors.request.use((config) => {
